d9f1a8a2036697b70c22caa8838f0f69114219b6
paweld <biuro@paweld.it>
2019-09-21 14:23:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mfrc522",
-    version="0.0.7",
+    version="0.0.8",
     author="Pi My Life Up",
     author_email="support@pimylifeup.com",
     description="A library to integrate the MFRC522 RFID readers with the Raspberry Pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pimylifeup/MFRC522-python",
+    url="https://github.com/paweld/MFRC522-python",
     packages=setuptools.find_packages(),
     install_requires=[
         'RPi.GPIO',
